534abd24ec8b6b527cdb01a6e9477df235480f6e
Nenartavičiūtė Laura <laura.nenartaviciute@mif.stud.vu.lt>
2022-05-27 11:47:50 +0300

    add beans, disappeared somehow

--- src/main/java/com/example/javaeefirst/usecases/ClientsOfShop.java
+++ src/main/java/com/example/javaeefirst/usecases/ClientsOfShop.java
@@ -14,14 +14,18 @@ import lombok.Setter;
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.persistence.OptimisticLockException;
 import javax.transaction.Transactional;
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-@Model
-public class ClientsOfShop {
+@Named
+@ViewScoped
+public class ClientsOfShop implements Serializable {
 
     @Inject
     private ClientsDAO clientsDAO;
@@ -46,6 +50,9 @@ public class ClientsOfShop {
     @Setter
     private ShoppingCart shoppingCartToCreate = new ShoppingCart();
 
+    @Getter @Setter
+    private String newShopAddress;
+
     @PostConstruct
     public void init() {
         Map<String, String> requestParameters =
@@ -66,8 +73,9 @@ public class ClientsOfShop {
     @Transactional
     public String updateShopAddress() {
         try{
-            Shop previoushop = shopsDAO.findOne(shop.getId());
-            previoushop.setAddress(shop.getAddress());
+           // Shop previousShop = shopsDAO.findOne(shop.getId());
+           // previousShop.setAddress(shop.getAddress());
+            shop.setAddress(newShopAddress);
             shopsDAO.update(shop);
         } catch (OptimisticLockException e) {
             return "/clients.xhtml?faces-redirect=true&shopId=" + this.shop.getId() + "&error=optimistic-lock-exception";
